945b6c273dbdd8ab808539d7cfc5d5e80159983b
Thomas-Bushido <thomas.pereira59@outlook.fr>
2025-10-08 11:09:47 +0200

    package.json updated + environments.ts updated + angular.json updated

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/refink-app-buyer-v2/browser')));
-
+app.use(express.static(path.join(__dirname, 'dist/refink-app-buyer-v2')));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/refink-app-buyer-v2/browser/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/refink-app-buyer-v2', 'index.html'));
 });
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://app-8c64c9fb-207f-4c42-af30-132121a3c14b.cleverapps.io'
+  apiUrl: 'https://app-8c64c9fb-207f-4c42-af30-132121a3c14b.cleverapps.io/api/refink/buyer'
 };
